cc497b47a55751448d3417db62cb398431ef3483
anaislet <anais.laura.letellier@gmail.com>
2022-06-18 16:07:25 +0200

    Poser un symbole en alternance avec gestion des conflits

---
+++ main.js
@@ -0,0 +1,30 @@
+const cases = document.getElementsByClassName('cliquable')
+console.log(cases)
+
+for (let uneCase of cases){
+    uneCase.addEventListener('click', jouer)
+}
+
+let symbole = "X"
+
+function jouer(e){
+    const laCase = e.target
+    // si la case cliquée est vide
+    if (laCase.innerHTML == ""){
+        // on remplit la case avec un symbole
+        laCase.innerHTML = symbole
+        // opn change de joueur donc de symbole
+        if (symbole == "X"){
+            symbole = "O"
+        }
+        else {
+            symbole = "X"
+        }
+    }
+    // si la case cliquée n'est pas vide
+    else {
+        console.error("NON !")
+    }
+
+}
+
