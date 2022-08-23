65ee5e71343a079e3eeed1720ea11c4c167a3c88
anaislet <anais.laura.letellier@gmail.com>
2022-08-23 21:19:27 +0200

    détection de la fin de partie, empêcher le clic après la fin de partie (#1)

--- main.js
+++ main.js
@@ -10,51 +10,56 @@ let score1 = 0
 let score2 = 0
 
 function jouer(e){
-    const symbole = document.querySelector('input[name="symbole' + joueurQuiJoue + '"]:checked').value
-    const laCase = e.target
-    // si la case cliquée est vide
-    if (laCase.textContent == ""){
-        tours = tours + 1
-        const inputs = document.querySelectorAll('div#symbole1 input, div#symbole2 input')
-        for (let input of inputs){
-            input.disabled = true
-        }
-        // on remplit la case avec un symbole
-        laCase.textContent = symbole
-        // on regarde si le joueur qui a joué a gagné
-        if(verifierPresenceGagnant()){
-            if (joueurQuiJoue == 1){
-                score1 = score1 + 1
-                const unite = score1 > 1 ? " points" : " point"
-                document.getElementById("score1").innerHTML = score1 + unite
-            }
-            else{
-                score2 = score2 + 1
-                const unite = score2 > 1 ? " points" : " point"
-                document.getElementById("score2").innerHTML = score2 + unite
-            }
-            document.querySelector('div#popup p.texte').innerHTML = "Vous avez gagné joueur " + document.getElementById("nomJoueur" + joueurQuiJoue).value
-            document.getElementById("popup").style.display = 'flex'
-        }
-        else if (tours == 9){
-            document.querySelector('div#popup p.texte').innerHTML = "Vous avez tous perdu"
-            document.getElementById("popup").style.display = 'flex'
-        }
-        // on change de joueur
-        if (joueurQuiJoue == 1){
-            joueurQuiJoue = 2
-            document.querySelector("section.joueur2").style.backgroundColor = "#2F2ABF"
-            document.querySelector("section.joueur1").style.backgroundColor = "#277BC0"
-        }
-        else {
-            joueurQuiJoue = 1
-            document.querySelector("section.joueur1").style.backgroundColor = "#2F2ABF"
-            document.querySelector("section.joueur2").style.backgroundColor = "#277BC0"
-        }
+    if (document.getElementById("popup").style.display == 'flex') {
+        console.log("partie terminée")
     }
-    // si la case cliquée n'est pas vide
     else {
-        console.log("NON !")
+        const symbole = document.querySelector('input[name="symbole' + joueurQuiJoue + '"]:checked').value
+        const laCase = e.target
+        // si la case cliquée est vide
+        if (laCase.textContent == ""){
+            tours = tours + 1
+            const inputs = document.querySelectorAll('div#symbole1 input, div#symbole2 input')
+            for (let input of inputs){
+                input.disabled = true
+            }
+            // on remplit la case avec un symbole
+            laCase.textContent = symbole
+            // on regarde si le joueur qui a joué a gagné
+            if(verifierPresenceGagnant()){
+                if (joueurQuiJoue == 1){
+                    score1 = score1 + 1
+                    const unite = score1 > 1 ? " points" : " point"
+                    document.getElementById("score1").innerHTML = score1 + unite
+                }
+                else{
+                    score2 = score2 + 1
+                    const unite = score2 > 1 ? " points" : " point"
+                    document.getElementById("score2").innerHTML = score2 + unite
+                }
+                document.querySelector('div#popup p.texte').innerHTML = "Vous avez gagné " + document.getElementById("nomJoueur" + joueurQuiJoue).value
+                document.getElementById("popup").style.display = 'flex'
+            }
+            else if (tours == 9){
+                document.querySelector('div#popup p.texte').innerHTML = "Vous avez tous perdu"
+                document.getElementById("popup").style.display = 'flex'
+            }
+            // on change de joueur
+            if (joueurQuiJoue == 1){
+                joueurQuiJoue = 2
+                document.querySelector("section.joueur2").style.backgroundColor = "#2F2ABF"
+                document.querySelector("section.joueur1").style.backgroundColor = "#277BC0"
+            }
+            else {
+                joueurQuiJoue = 1
+                document.querySelector("section.joueur1").style.backgroundColor = "#2F2ABF"
+                document.querySelector("section.joueur2").style.backgroundColor = "#277BC0"
+            }
+        }
+        // si la case cliquée n'est pas vide
+        else {
+            console.log("NON !")
+        }
     }
 }
 
